4fc39afa96d06c4d988e7b2b0bd03e98a3da7524
DDongHyun00 <117076978+DDongHyun00@users.noreply.github.com>
2025-03-16 11:50:06 +0900

    .

--- src/UserInput.java
+++ src/UserInput.java
@@ -6,6 +6,7 @@ public class UserInput {
         Scanner sc = new Scanner(System.in);
         System.out.printf("오늘의 감정을 입력하세요. \n =>");
         situation = sc.nextLine();
+        sc.close();
         return situation;
     }
 }
