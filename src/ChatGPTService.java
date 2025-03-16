1397faee58129aefa6d369b86d8e12a29db47a97
DDongHyun00 <117076978+DDongHyun00@users.noreply.github.com>
2025-03-16 11:56:18 +0900

    .

--- src/ChatGPTService.java
+++ src/ChatGPTService.java
@@ -2,4 +2,8 @@ public class ChatGPTService {
     private static final String API_KEY = "";
     private static final String API_URL = "https://api.openai.com/v1/chat/completions";
 
+    public static String getEmotion(String userMood) throws Exception{
+
+        return "";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import org.json.simple.JSONArray;
-
 public class Main {
     public static void main(String[] args) {
 
