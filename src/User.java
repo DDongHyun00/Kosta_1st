fb31dc55dc295df1bf347cb58e157e0c81a98934
DDongHyun00 <117076978+DDongHyun00@users.noreply.github.com>
2025-03-20 10:39:25 +0900

    User정보 getter 추가

--- src/User.java
+++ src/User.java
@@ -1,16 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class User {
-    private final int uniqueNumber; // 개인 고유번호
-    private static int userCount;
+    private final int uniqueNumber = 0; // 개인 고유번호
+    private static int userCount = 0; //고유번호 ++
     private String name; // 이름
     private String gender; // 성별
     private int age; // 나이
     private int userid; // 아이디
     private int passwd; // 비밀번호
+    private String userInput; // 사용자가 입력한 감정 문장
 
-    public User(int uniqueNumber) {
-        this.uniqueNumber = uniqueNumber;
+    private static final Map<String, User> userDatabase = new HashMap<>();
+
+
+
+
+    public int getage(){
+        return age;
+    }
+    public String getgender(){
+        return gender;
     }
 
 
