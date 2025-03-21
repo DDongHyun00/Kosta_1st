f84201a7607b330ee224484dfe2ab54dc809a01a
DDongHyun00 <117076978+DDongHyun00@users.noreply.github.com>
2025-03-21 10:29:58 +0900

    메뉴 반복 기능 개선

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 
 public class Main {
-//    private static User loggedInUser;
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
