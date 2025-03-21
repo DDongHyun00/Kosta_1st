90bfbe5b2c944f6128a8960e1c6bc186459fac5f
DESKTOP-8MAM7KK\wd <bambin25>
2025-03-21 09:36:23 +0900

    registerUser ver_0.4

---
+++ src/PlayList.java
@@ -0,0 +1,8 @@
+public class PlayList {
+
+  // 목적 - 플레이리스트에 gpt한테 받아온 값 넣어놓기
+  // 제목 , 유튜브 링크, 감정입력값(String) , 감정분석값 (String)
+  //private static final Map<String, User> userDatabase = new HashMap<>(); // 회원정보 저장
+
+
+}
